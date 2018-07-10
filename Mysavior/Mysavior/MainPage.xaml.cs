9c5b624241e10741a8e5202952a4f2b36dacb5a5
ccrrissttianno <cristiano_rodrigues@outlook.com>
2018-07-10 16:11:43 -0300

    Initial commit

---
+++ Mysavior/Mysavior/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Mysavior
+{
+	public partial class MainPage : ContentPage
+	{
+        public MainPage()
+        {
+            InitializeComponent();
+            Browser.HeightRequest = 1000;
+            Browser.WidthRequest = 1000;
+            Browser.Source = "https://mysavior.com.br";
+
+        }
+
+    }
+	
+}
